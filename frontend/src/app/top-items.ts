53e1de5e58191b29de916eed55435adaf5d2441d
Bencsik András Pál <h253637@stud.u-szeged.hu>
2025-08-12 21:29:08 +0200

    feat: trying to solve session differences between angular and flask

--- frontend/src/app/top-items.ts
+++ frontend/src/app/top-items.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
 import { Item } from './item.data';
 
 
@@ -9,11 +10,20 @@ import { Item } from './item.data';
   providedIn: 'root'
 })
 export class TopItems {
-  private apiUrl = 'http://localhost:5000/get-data/top-tracks';
+  private baseUrl = 'http://127.0.0.1:5000';
+  private apiUrl = `${this.baseUrl}/get-data/top-tracks`;
 
   constructor(private http: HttpClient) { }
 
   getTopItems(): Observable<Item[]> {
-    return this.http.get<Item[]>(this.apiUrl);
+    return this.http.get<Item[]>(this.apiUrl, {withCredentials: true});
+  }
+
+  checkAuthStatus(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/auth/status`, { withCredentials: true });
+  }
+
+  login(): void {
+    window.location.href = `${this.baseUrl}/auth/login`;
   }
 }
